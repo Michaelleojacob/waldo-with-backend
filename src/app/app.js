fb52e4db1dd361ab4d93ce3ea795300cbc66f717
mig <michaelleojacob@gmail.com>
2022-05-22 21:22:42 -0700

    next goal is to add CRUD to the firebase Data

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,8 @@
 const App = () => {
-  const handleClick = () => console.log('hi');
-
   return (
     <div>
       <div>hi from app</div>
       <div>dude wtf</div>
-      <button onClick={handleClick}>click to add data to firebase</button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/app';
+import app from './app/utils/firebase';
+
+console.log(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
