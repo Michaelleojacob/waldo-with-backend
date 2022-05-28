1479318cc5a5e291e8f217f6b5589fd9ce25aedc
mig <michaelleojacob@gmail.com>
2022-05-28 11:20:14 -0700

    updated firestore.js, removed all the commented code and old v8 examples from the codelab. I will just google that info should I need it.

--- src/app/firebase-utils/firestore.js
+++ src/app/firebase-utils/firestore.js
@@ -1,61 +1,3 @@
-// import db from './firestore.js';
-// import {
-//   doc,
-//   setDoc,
-//   addDoc,
-//   collection,
-//   getDoc,
-//   getDocs,
-//   updateDoc,
-//   deleteDoc,
-// } from 'firebase/firestore';
-
-// const createnote = async (note) => {
-//   await setDoc(doc(db, 'messages', note.id), note);
-// };
-
-// const createNote = async (note) => {
-//   await addDoc(collection(db, 'notes'), note);
-// };
-
-// const getNote = async (id) => {
-//   const noteSnapshot = await getDoc(doc(db, 'notes', id));
-//   if (noteSnapshot.exists()) {
-//     return noteSnapshot.data();
-//   } else {
-//     console.log("Note doesn't exist");
-//   }
-// };
-
-// const getNotes = async () => {
-//   const notesSnapshot = await getDocs(collection(db, 'notes'));
-//   const notesList = notesSnapshot.docs.map((doc) => doc.data());
-//   return notesList;
-// };
-
-// const updateNote = async (note) => {
-//   const noteRef = doc(db, 'notes', note.id);
-//   await updateDoc(noteRef, {
-//     description: 'New description',
-//   });
-// };
-
-// const deleteNote = async (note) => {
-//   const noteRef = doc(db, 'notes', note.id);
-//   await deleteDoc(noteRef);
-// };
-
-// const store = {
-//   createnote,
-//   createNote,
-//   getNote,
-//   getNotes,
-//   updateNote,
-//   deleteNote,
-// };
-
-// export default store;
-
 import { db } from './firebase';
 
 import {
@@ -66,6 +8,7 @@ import {
   getDoc,
   serverTimestamp,
   updateDoc,
+  setDoc,
 } from 'firebase/firestore';
 
 export const createTempUser = async () => {
@@ -87,14 +30,6 @@ export const createTempUser = async () => {
   }
 };
 
-export const logGameOne = async () => {
-  const querySnapShot = await getDocs(collection(db, 'gameOneChars'));
-  querySnapShot.forEach((doc) => {
-    console.log(doc.id);
-    console.log(doc.data());
-  });
-};
-
 export const getCharCoords = async (gameNum, charNum) => {
   const selectedGame = gameNum === 1 ? 'gameOneChars' : 'gameTwoChars';
   const docRef = doc(db, selectedGame, charNum);
@@ -102,39 +37,6 @@ export const getCharCoords = async (gameNum, charNum) => {
   const { x, y } = docSnap.data();
   return { x, y };
 };
-
-// (above) updated the example to get just a single doc, instead of a collection.
-// (below) old attempt with getCharCoords.
-
-// export const getCharCoords = async (gameNum, charNum) => {
-//   const coords = {};
-//   const selectedGame = gameNum === 1 ? 'gameOneChars' : 'gameTwoChars';
-//   const querySnapShot = await getDocs(collection(db, selectedGame));
-//   querySnapShot.forEach((item) => {
-//     if (item.id === charNum) {
-//       coords.x = item.data().x;
-//       coords.y = item.data().y;
-//     }
-//   });
-//   return coords;
-// };
-
-//===========================================================================
-
-// export const removeOldTempUsers = async () => {
-//   const currentTime = Date.now();
-//   const getTempUsers = await getDocs(collection(db, 'tempUsers'));
-//   const cutoff = currentTime - 2 * 60 * 60 * 1000;
-// };
-
-// const getNote = async (id) => {
-//   const noteSnapshot = await getDoc(doc(db, 'notes', id));
-//   if (noteSnapshot.exists()) {
-//     return noteSnapshot.data();
-//   } else {
-//     console.log("Note doesn't exist");
-//   }
-// };
 
 export const getTempUser = async (id) => {
   const userSnap = await getDoc(doc(db, 'tempUsers', id));
@@ -149,16 +51,3 @@ export const updateTempUser = async (id) => {
     name: 'lol',
   });
 };
-
-// const frankDocRef = doc(db, "users", "frank");
-// await setDoc(frankDocRef, {
-//     name: "Frank",
-//     favorites: { food: "Pizza", color: "Blue", subject: "recess" },
-//     age: 12
-// });
-
-// // To update age and favorite color:
-// await updateDoc(frankDocRef, {
-//     "age": 13,
-//     "favorites.color": "Red"
-// });
